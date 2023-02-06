a9c84ee8c42746a837bcd488f70dab736f9f71df
Ollieux <oliwier.pszeniczko@gmail.com>
2023-02-07 00:44:45 +0100

    webcam_fps.py rotation off pc

--- Webcam/webcam_fps.py
+++ Webcam/webcam_fps.py
@@ -13,7 +13,7 @@ while True:
     _, frame = webcam.read()
     # frame = cv.resize(frame, (width, height))
     # image = cv.rotate(frame, cv.ROTATE_90_COUNTERCLOCKWISE)
-    image = cv.rotate(frame, cv.ROTATE_90_CLOCKWISE)
+    # image = cv.rotate(frame, cv.ROTATE_90_CLOCKWISE)
     image = cv.putText(image, 'FPS: ' + str(fps), (10, 40), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
     cv.imshow("Widok", image)
     # cv.imshow("Widok", frame)
