8c3f273f43e6d354ecb94b43f37b16483f8c3e33
Ollieux <oliwier.pszeniczko@gmail.com>
2023-02-09 13:43:02 +0100

    fire_detection_class.py CascadeClassifier full path /home instead tilda and gray pc

--- Fire/fire_detection_class.py
+++ Fire/fire_detection_class.py
@@ -7,15 +7,16 @@ import numpy as np
 # fire_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'fire_detection.xml')
 # fire_cascade = cv2.CascadeClassifier('~/Rpi-Repo/Fire/fire_detection.xml')
 # fire_cascade = cv2.CascadeClassifier("~/Rpi-Repo/Fire/fire_detection.xml")
-fire_cascade = cv2.CascadeClassifier("/home/Rpi-Repo/Fire/fire_detection.xml")
+
+fire_cascade = cv2.CascadeClassifier('/home/Rpi-Repo/Fire/fire_detection.xml')
 
 # cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture("http://192.168.1.31:8081/")
+cap = cv2.VideoCapture('http://192.168.1.31:8081/')
 
 while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    fire = fire_cascade.detectMultiScale(frame, 1.2, 5)
+    fire = fire_cascade.detectMultiScale(gray, 1.2, 5)
 
     for (x, y, w, h) in fire:
         cv2.rectangle(frame, (x - 20, y - 20),(x + w + 20, y + h + 20), (255, 0, 0), 2)
