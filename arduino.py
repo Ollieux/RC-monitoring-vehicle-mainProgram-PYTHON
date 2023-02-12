fe6321cb46a342a5a9fda4db8cc41553e0889de8
Ollieux <oliwier.pszeniczko@gmail.com>
2023-02-12 01:40:53 +0100

    change serial path arduino.py pc

--- arduino.py
+++ arduino.py
@@ -1,7 +1,7 @@
 import serial
 
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 ser.reset_input_buffer()
 while True:
     if ser.in_waiting > 0:
