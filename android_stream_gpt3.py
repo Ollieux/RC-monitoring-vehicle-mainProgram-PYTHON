ea123ac2986fe39b8bc8f657a6073b4310a511d9
Ollieux <oliwier.pszeniczko@gmail.com>
2023-02-13 16:13:42 +0100

    add rotate android_stream_gpt3.py pc

--- android_stream_gpt3.py
+++ android_stream_gpt3.py
@@ -7,6 +7,9 @@ port = 9999
 
 cap = cv2.VideoCapture(0)
 
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 320)
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((host, port))
 server_socket.listen(0)
@@ -15,6 +18,9 @@ connection, address = server_socket.accept()
 
 while True:
     ret, frame = cap.read()
+
+    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+
     data = cv2.imencode('.jpg', frame)[1].tobytes()
     connection.sendall(struct.pack("!i", len(data)) + data)
 
