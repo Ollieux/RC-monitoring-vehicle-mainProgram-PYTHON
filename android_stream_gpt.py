eec7df34ca710117a74a52f8d35ef7c65beac33f
Ollieux <oliwier.pszeniczko@gmail.com>
2023-02-13 14:46:42 +0100

    add file android_stream_gpt2.py pc

--- android_stream_gpt.py
+++ android_stream_gpt.py
@@ -4,10 +4,12 @@ import numpy as np
 
 # Create a socket connection
 server_socket = socket.socket()
+# server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "192.168.1.31"
-port = 8001
+port = 12345
 server_socket.bind((host, port))
-server_socket.listen(1)
+# server_socket.listen(1)
+server_socket.listen(5)
 print("Waiting for a connection...")
 connection, address = server_socket.accept()
 print("Accepted connection from", address)
---
+++ android_stream_gpt2.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+import socket
+import struct
+
+host = "192.168.1.31"
+port = 12345
+
+server_socket = socket.socket()
+server_socket.bind((host, port))
+server_socket.listen(0)
+
+connection, address = server_socket.accept()
+connection = connection.makefile("rb")
+
+cam = cv2.VideoCapture(0)
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+    result, encimg = cv2.imencode('.jpg', frame, encode_param)
+    data = np.array(encimg)
+    stringData = data.tobytes()
+
+    connection.write(struct.pack("<L", len(stringData)) + stringData)
+
+cam.release()
+cv2.destroyAllWindows()
