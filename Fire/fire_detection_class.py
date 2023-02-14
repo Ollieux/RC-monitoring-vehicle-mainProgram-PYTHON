3af77f1d618438248e658235220b1e13abe397a8
Ollieux <oliwier.pszeniczko@gmail.com>
2023-02-14 16:19:21 +0100

    change to minimal fire_detection_class.py pc add wifi and additional  README.md

--- Fire/fire_detection_class.py
+++ Fire/fire_detection_class.py
@@ -1,25 +1,63 @@
+import time
 import cv2
 import numpy as np
 
-fire_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'fire_detection.xml')
 
-# cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture('http://192.168.1.31:8081/')
+if __name__ == '__main__':
 
-while(True):
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    fire = fire_cascade.detectMultiScale(gray, 1.2, 5)
+    # fire_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'fire_detection.xml')
+    fire_cascade = cv2.CascadeClassifier('fire_detection.xml')
 
-    for (x, y, w, h) in fire:
-        cv2.rectangle(frame, (x - 20, y - 20),(x + w + 20, y + h + 20), (255, 0, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
-        roi_color = frame[y:y + h, x:x + w]
-        print('fire is detected')
+    cap = cv2.VideoCapture(0)
+    # cap = cv2.VideoCapture('http://192.168.1.31:8081/')
 
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
-cap.release()
-cv2.DestroyAllWindows()
+    width =  160 # 240
+    height = 80  # 160
+
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+    print("width: ", cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    print("height: ", cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print("fps: ", cap.get(cv2.CAP_PROP_FPS))
+
+    fps, st, frames_to_count, cnt = (0, 0, 30, 0)
+
+    while(True):
+        ret, frame = cap.read()
+
+        # frame = cv2.resize(frame, (width, height))
+        frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+
+        # print("fps: ", cap.get(cv2.CAP_PROP_FPS))
+
+
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        fire = fire_cascade.detectMultiScale(gray, 1.3, 5)
+
+        for (x, y, w, h) in fire:
+            # cv2.rectangle(frame, (x - 20, y - 20),(x + w + 20, y + h + 20), (255, 0, 0), 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            # roi_gray = gray[y:y + h, x:x + w]
+            # roi_color = frame[y:y + h, x:x + w]
+            print('fire is detected')
+
+        frame = cv2.putText(frame, 'FPS: ' + str(fps), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        if cnt == frames_to_count:
+            try:
+                fps = round(frames_to_count / (time.time() - st))
+                st = time.time()
+                cnt = 0
+                print(fps)
+            except:
+                pass
+        cnt += 1
+
+    cap.release()
+    cv2.DestroyAllWindows()
--- README.md
+++ README.md
@@ -1,32 +1,58 @@
 # Rpi-Repo
 
 ## history:
-echo 'HISTTIMEFORMAT="%d/%m/%y %T "' >> ~/.bashrc\
+echo 'HISTTIMEFORMAT="%d/%m/%y %T "' >> ~/.bashrc \
 source ~/.bashrc
 
 ## ssh:
-mkdir .ssh\
-ssh-keygen -t rsa -C "oliwier.pszeniczko@gmail.com" -f "id_rsa_ollieux_rpi"\
-eval "$(ssh-agent -s)"\
-ssh-add ~/.ssh/id_rsa_ollieux_rpi\
+mkdir .ssh \
+ssh-keygen -t rsa -C "oliwier.pszeniczko@gmail.com" -f "id_rsa_ollieux_rpi" \
+eval "$(ssh-agent -s)" \
+ssh-add ~/.ssh/id_rsa_ollieux_rpi \
 ssh -T git@github.com
 
 ## catalog -> git:
 git init\
-git remote add origin <>\
-git config user.email oliwier.pszeniczko@gmail.com\
-git config user.name Ollieux\
+git remote add origin <> \
+git config user.email oliwier.pszeniczko@gmail.com \
+git config user.name Ollieux \
 git pull origin main
 
 ## git -> catalog:
-git clone http:<>\
-git config user.email oliwier.pszeniczko@gmail.com\
-git config user.name Ollieux\
+git clone http:<> \
+git config user.email oliwier.pszeniczko@gmail.com \
+git config user.name Ollieux \
 
 ##Open-cv:
+sudo apt-get install python3-pip python3-virtualenv
+
+mkdir project \
+cd project/
+virtualenv env
+
+sudo apt install -y build-essential cmake pkg-config libjpeg-dev libtiff5-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libhdf5-dev libhdf5-serial-dev libhdf5-103 libqt5gui5 libqt5webkit5 libqt5test5 python3-pyqt5 python3-dev
+
+pip install --upgrade pip setuptools wheel
+
 pip install opencv-python==4.5.3.56
 
 ## Owner change:
 git rebase -i --root\
 git commit --amend --author="Ollieux <oliwier.pszeniczko@gmail.com>" --no-edit\
 git rebase --continue\
+
+## Wifi
+sudo apt-get install -y linux-headers \
+git clone https://github.com/lwfinger/rtl8188eu.git \
+cd rtl8188eu/ \
+make \
+sudo make install \
+sudo reboot \
+sudo nano /etc/network/interfaces \
+sudo nano /etc/wpa_supplicant.conf
+
+## additonal
+sudo apt-get install conky \
+pip install imutils
+
+
