5d800525221a1136b77f95449f901f45fa15e180
Ollieux <oliwier.pszeniczko@gmail.com>
2023-02-15 14:25:32 +0100

    llevel solution fire_detection_class.py pc

--- Fire/fire_detection_class.py
+++ Fire/fire_detection_class.py
@@ -12,11 +12,13 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
     # cap = cv2.VideoCapture('http://192.168.1.31:8081/')
 
-    _width =  320 # 176# ## #  240
-    _height =  240 #144 ###  #  160
+    _width = 320 # 176# ## #  240
+    _height = 240 #144 ###  #  160
 
-    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, _width)
-    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, _height)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, _width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, _height)
+
+    # cap.set(cv2.CAP_PROP_FPS, 10)
 
     print("width: ", cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     print("height: ", cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -27,7 +29,7 @@ if __name__ == '__main__':
     while(True):
         ret, frame = cap.read()
 
-        frame = cv2.resize(frame, (_width, _height))
+        # frame = cv2.resize(frame, (_width, _height))
         #
         frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
 
@@ -45,8 +47,8 @@ if __name__ == '__main__':
             # roi_color = frame[y:y + h, x:x + w]
             print('fire is detected')
 
-        # frame = cv2.putText(frame, 'FPS: ' + str(fps), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        # cv2.imshow('frame', frame)
+        frame = cv2.putText(frame, 'FPS: ' + str(fps), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.imshow('frame', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
