77a0147ac067b154767bb48c7402a3e8c38b50b2
Ollieux <oliwier.pszeniczko@gmail.com>
2023-02-15 12:04:21 +0100

    test hlevel solution fire_detection_class.py pc

--- Fire/fire_detection_class.py
+++ Fire/fire_detection_class.py
@@ -1,25 +1,26 @@
 import time
 import cv2
+import imutils
 import numpy as np
 
 
 if __name__ == '__main__':
 
-    fire_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'fire_detection.xml')
-    # fire_cascade = cv2.CascadeClassifier('fire_detection.xml')
+    # fire_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'fire_detection.xml')
+    fire_cascade = cv2.CascadeClassifier('fire_detection.xml')
 
     cap = cv2.VideoCapture(0)
     # cap = cv2.VideoCapture('http://192.168.1.31:8081/')
 
-    width =  240 # 240 160
-    height = 160  # 160
+    width =  320 #176# #  240
+    height =   240#144#  #  160
 
-    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    #cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    #cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
     print("width: ", cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     print("height: ", cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    print("fps: ", cap.get(cv2.CAP_PROP_FPS))
+    # print("fps: ", cap.get(cv2.CAP_PROP_FPS))
 
     fps, st, frames_to_count, cnt = (0, 0, 30, 0)
 
@@ -27,11 +28,11 @@ if __name__ == '__main__':
         ret, frame = cap.read()
 
         frame = cv2.resize(frame, (width, height))
+
         frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
 
-        # print("fps: ", cap.get(cv2.CAP_PROP_FPS))
-
-
+        #frame = imutils.resize(frame, width=176, height=144)
+        #frame = imutils.rotate_bound(frame, 90)
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         fire = fire_cascade.detectMultiScale(gray, 1.3, 5)
