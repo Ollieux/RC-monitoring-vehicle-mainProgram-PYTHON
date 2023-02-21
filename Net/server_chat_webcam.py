87fab3e88e10d254727e9278fd16d7958568e489
Ollieux <oliwier.pszeniczko@gmail.com>
2023-02-21 16:42:12 +0100

    version sending image receiving message single connection server_chat_webcam.py

---
+++ Net/server_chat_webcam.py
@@ -0,0 +1,80 @@
+import pickle
+import socket
+import struct
+import threading
+import cv2
+import imutils
+# --- functions ---
+
+
+
+def recive_data():
+    while True:
+        data = conn.recv(1024).decode("utf-8")
+        if not data:
+            print("null")
+            break
+        # data = data.decode().decode("utf-8")
+        print(data)
+
+def send_frame():
+        vid = cv2.VideoCapture(0)
+        while vid.isOpened():
+            ret, frame = vid.read()
+            frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            data = cv2.imencode('.jpg', frame)[1].tobytes()
+            try:
+
+                conn.sendall(struct.pack("!i", len(data)) + data)
+            except Exception as e:
+                print(e)
+                break
+            # frame = imutils.resize(frame, width=320)
+            # frame = imutils.rotate_bound(frame, angle=-90)
+            # a = pickle.dumps(frame)
+            # message = struct.pack("Q", len(a)) + a
+            # conn.sendall(message)
+            #
+
+            # cv2.imshow('TRANSMITTING VIDEO', frame)
+            # key = cv2.waitKey(1) & 0xFF
+            # if key == ord('q'):
+            #     conn.close()
+
+# --- main ---
+
+host = '192.168.1.5'
+port = 9977
+
+s = socket.socket()
+s.bind((host, port))
+s.listen(1)
+
+#while True:
+print("Waiting for connection...")
+conn, addr = s.accept()
+print(addr, " connected")
+
+    # t
+receive_thread = threading.Thread(target=recive_data, )
+receive_thread.start()
+
+    # t2
+send_thread = threading.Thread(target=send_frame, )
+send_thread.start()
+
+receive_thread.join()
+send_thread.join()
+
+
+# send_thread = threading.Thread(target=send_frame, )
+# send_thread.start()
+#
+# receive_thread = threading.Thread(target=recive_data, )
+# receive_thread.start()
+#
+# send_thread.join()
+# receive_thread.join()
+
+
+print("end")
