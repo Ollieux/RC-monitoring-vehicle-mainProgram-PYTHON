e05bd6145a135bc94d5bcf741766b1b8bceb4708
Ollieux <oliwier.pszeniczko@gmail.com>
2023-03-07 00:53:54 +0100

    retest smoke sensor main.py

--- button.py
+++ button.py
@@ -8,9 +8,9 @@ def button_callback(channel):
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.add_event_detect(18, GPIO.RISING, callback=button_callback)
+GPIO.add_event_detect(17, GPIO.RISING, callback=button_callback)
 
 message = input("Press enter to quit\n\n")
 GPIO.cleanup()
