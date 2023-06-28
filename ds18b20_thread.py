50c8779be19c4fb9709d798adfb2be17fcf07908
Ollieux <oliwier.pszeniczko@gmail.com>
2023-06-28 17:45:54 +0200

    fix minor ds18b20_thread.py

--- ds18b20_thread.py
+++ ds18b20_thread.py
@@ -5,6 +5,7 @@ import queue
 import threading
 
 
+
 sensor = w1thermsensor.W1ThermSensor()
 temp_queue = queue.Queue()
 
@@ -15,15 +16,20 @@ def temp():
         temp_queue.put(temperatura)
         time.sleep(1)
 
+print("hello1")
+
 temp_thread = threading.Thread(target=temp, )
 temp_thread.start()
 
 while(True):
-    read = temp_queue.get(block=False)
-    print(read)
+    try:
+        read = temp_queue.get(block=False)
+        print(read)
+    except queue.Empty:
+        print(":(")
 
 
-print("hello")
+print("hello2")
 
 
 
