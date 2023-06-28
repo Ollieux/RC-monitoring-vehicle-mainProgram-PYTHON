9d154353b8deca0bb8385134db7b470776a2eb3d
Ollieux <oliwier.pszeniczko@gmail.com>
2023-06-28 17:26:51 +0200

    implement queue ds18b20_thread.py

--- ds18b20_thread.py
+++ ds18b20_thread.py
@@ -2,17 +2,24 @@ import time
 import RPi.GPIO as GPIO
 import w1thermsensor
 import threading
+import queue
 
 sensor = w1thermsensor.W1ThermSensor()
+temp_queue = queue.Queue()
 
 def temp():
     while True:
         temperatura = sensor.get_temperature()
-        print(temperatura)
+        # print(temperatura)
+        temp_queue.put(temperatura)
         time.sleep(1)
 
 temp_thread = threading.Thread(target=temp, )
 temp_thread.start()
+
+while(True):
+    read = temp_queue.get(block=False)
+    print(read)
 
 
 print("hello")
