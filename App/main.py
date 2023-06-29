91de794fbae86ace54d4b0befbca80bfeb454dcf
Ollieux <oliwier.pszeniczko@gmail.com>
2023-06-29 13:51:47 +0200

    Version commentless & printless

--- App/main.py
+++ App/main.py
@@ -13,8 +13,6 @@ import w1thermsensor
 from firebase_admin import credentials, messaging
 
 
-
-
 def send_notification(factor, _):
 
     title = "Warning!"
@@ -37,14 +35,7 @@ def send_notification(factor, _):
         ),
         token=registration_token,
     )
-    #TODO: final out
-    response = messaging.send(msg)
-    print('Successfully sent message:', response)
 
-def button_callback(channel):
-
-    print("Button was pushed!")
-    os.system("sudo reboot")
 
 def send_controls():
 
@@ -59,11 +50,14 @@ def send_controls():
 def receive_data():
 
     while True:
+
         try:
             data = connection.recv(SIZE).decode("utf-8")
             if not data:
+
                 print("null")
                 break
+
             print(data)
             data_queue.put(data)
         except Exception as e:
@@ -77,6 +71,7 @@ def send_frame():
     temp = gtemperature
 
     while True:
+
         sframe = send_frame_queue.get()
 
         try:
@@ -93,10 +88,8 @@ def send_frame():
         try:
             data = cv2.imencode('.jpg', sframe)[1].tobytes()
             connection.sendall(struct.pack("!i", len(data)) + data)
-            # conn.sendall(struct.pack("!i", len(data)) + data)
         except Exception as e:
             print(e)
-            # conn.close()
             break
 
 
@@ -108,29 +101,13 @@ def capture_frame():
     cap.set(3, WIDTH)
     cap.set(4, HEIGHT)
     cap.set(5, FPS)
-    # while True:
-        # try:
     capturing = True
     while cap.isOpened():
 
         ret, frame = cap.read()
-
-        #TODO?: tutaj resize imutils/cv
-        # frame = imutils.resize(frame, (width=WIDTH, height=HEIGHT)) #
-        # frame = cv2.rotate(frame, (width=WIDTH, height=HEIGHT)) ##
-        # moze sie klocic z podwojnym resizem w przypadku koniecznosci resize dla detect
-
-
-        #TODO?: tutaj rotate, imutils/cv
-        # frame = imutils.rotate_bound(frame, angle=-90)
-
         frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
         if detecting:
-
-            #TODO?: tutaj resize dla detect
-            # imutils.resize(frame, (width=176, height=144))
-            # frame = cv2.resize(frame, (176, 144))
 
             detect_frame_queue.put(frame)
 
@@ -142,30 +119,28 @@ def capture_frame():
         cv2.imshow("Capture", frame)
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
+
             break
 
         if not running:
+
             break
 
-    # except Exception as e:
-        # print("Error in capture_frames:", e)
     capturing = False
     cap.release()
-        # break
 
 
 def detect_smoke():
 
     while True:
 
-        # if not GPIO.input(5):
         if not GPIO.input(18):
 
             global smoke_notified, smoke_time
-
             if smoke_notified:
 
                 if time.time() - smoke_time > NOTIFICATION_TIMER:
+
                     smoke_notified = False
 
             if not smoke_notified:
@@ -173,7 +148,6 @@ def detect_smoke():
                 threading.Thread(target=send_notification, args=("smoke", "")).start()
                 smoke_time = time.time()
                 smoke_notified = True
-
 
         time.sleep(1)
 
@@ -198,12 +172,9 @@ def detect_fire():
 
             if fire_notified:
 
-                # if not time.time() - fire_time > FIRE_TIMER:
                 if time.time() - fire_time > NOTIFICATION_TIMER:
-                    fire_notified = False
-                    # break
 
-            # fire_notified = False
+                    fire_notified = False
 
             if not fire_notified:
 
@@ -218,37 +189,38 @@ def detect_fire():
             detecting = False
             break
 
+
 def read_temp():
 
     while True:
+
         while connected:
+
             read = sensor.get_temperature()
             temp_queue.put(read)
             time.sleep(1)
 
+        # TODO: sleep(0.5) ?
 
 
 def button_callback(channel):
-    print("Button was pushed!")
-    GPIO.cleanup();
+
+    global running
+    running = False
+    GPIO.cleanup()
     os.system("sudo shutdown -h now")
 
+
 GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BCM)  # BCM numbering, not BOARD
-# GPIO.setup(5, GPIO.IN)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(17, GPIO.RISING, callback=button_callback)
-# GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 sensor = w1thermsensor.W1ThermSensor()
 
 host = socket.gethostbyname(socket.gethostname() + ".local")
-#TODO:
-# final out
-print("IP address of the localhost is {}".format(host))
 
 port = 9977
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -265,8 +237,8 @@ registration_token = 'epZx5w_RToGbXxrpEjeMXN:APA91bG2_S8rKS3enFhMq9oHwBoJt_XYn4n
 WIDTH = 320
 HEIGHT = 240
 FPS = 10
-SIZE = 96 # 1024
-NOTIFICATION_TIMER = 30 #300
+SIZE = 96
+NOTIFICATION_TIMER = 30
 
 capturing = False
 detecting = False
@@ -275,7 +247,7 @@ fire_notified = False
 smoke_notified = False
 fire_time = 0
 smoke_time = 0
-gtemperature = 0 # sensor.get_temperature()
+gtemperature = 0
 running = True
 
 send_frame_queue = queue.Queue()
@@ -283,14 +255,10 @@ detect_frame_queue = queue.Queue()
 data_queue = queue.Queue()
 temp_queue = queue.Queue()
 
-## capture_thread = threading.Thread(target=capture_frame, daemon=True)
+
 capture_thread = threading.Thread(target=capture_frame, )
 capture_thread.start()
 
-
-#time.sleep(10)
-#TODO: final out
-print("wait for capturing")
 
 while not capturing:
     
@@ -311,20 +279,14 @@ temp_thread.start()
 
 while True:
     try:
-        print("Waiting for connection")
         connection, client_address = server_socket.accept()
         connected = True
-        print(client_address, " connected")
         receive_thread = threading.Thread(target=receive_data, )
         receive_thread.start()
         send_thread = threading.Thread(target=send_frame, )
         send_thread.start()
-        #TODO: if not capturing:
-        # backup_capture_thread = threading.Thread(target=capture_frame, )
-        # backup_capture_thread.start()
         receive_thread.join()
         send_thread.join()
-        print("end connection")
         connected = False
 
     except KeyboardInterrupt as e:
@@ -333,13 +295,10 @@ while True:
         GPIO.cleanup()
         break
 
-
-## fire_thread.join()
-## capture_thread.join()
-
 #TODO: pre final
+# git checkout main
 # git checkout -b pc
-# git merge main
+# git merge main albo nie
 # git checkout main
 # git merge dev-rpi
 # TODOs final/final out
